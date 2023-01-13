cd068d547492e82d3d13c362d90e82002cf0c72a
g4knr <85592362+g4knr@users.noreply.github.com>
2023-01-13 16:21:47 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { greetUser } from '$utils/greet';
+import { simulateEvent } from '@finsweet/ts-utils';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const MODAL_BUTTONS = [...document.querySelectorAll('[data-modal-trigger]')];
+  MODAL_BUTTONS.forEach((BUTTON) => {
+    BUTTON.addEventListener('click', () => {
+      const TRIGGER = document.querySelector(`#${BUTTON.getAttribute('data-modal-trigger')}`);
+      console.log(TRIGGER);
+      simulateEvent(TRIGGER, 'click');
+    });
+  });
 });
