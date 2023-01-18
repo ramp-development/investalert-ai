49160aeb7f832f29ff600122d41232e4159d1efd
g4knr <85592362+g4knr@users.noreply.github.com>
2023-01-18 10:20:17 +0000

    Init

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import { simulateEvent } from '@finsweet/ts-utils';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const MODAL_BUTTONS = [...document.querySelectorAll('[data-modal-trigger]')];
-  MODAL_BUTTONS.forEach((BUTTON) => {
-    BUTTON.addEventListener('click', () => {
-      const TRIGGER = document.querySelector(`#${BUTTON.getAttribute('data-modal-trigger')}`);
-      console.log(TRIGGER);
-      simulateEvent(TRIGGER, 'click');
-    });
-  });
+  const accordionHeader = document.querySelector('.faqs_group-header');
+  simulateEvent(accordionHeader, 'click');
 });
